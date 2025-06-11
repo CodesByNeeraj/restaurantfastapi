475df21b7a85c7eb83ae7ab9cc32c9376b3c39b7
Neeraj Lakshmanan <neerajsubi@gmail.com>
2025-06-11 10:45:00 +0800

    feat: added in db retrieval code for frontend display

--- backend.js
+++ backend.js
@@ -3,6 +3,9 @@ function handleButton1Click(){
     var span1 = document.getElementById("span1");  //display restaurant name and menu 
     
     //fetch to make HTTP requests (below is the api aka HTTP endpoint)
+    //fetch is an api and replacement to xmlhttprequest
+    //the response is returned by the python fastapi backend. the response is json string
+    //we turn that json string into a javascript object
     fetch("http://127.0.0.1:8000/restaurant",{
         method: "POST", //post means sending data over to the server. we are sending user input over to the backend
         headers: {
@@ -28,9 +31,41 @@ function handleButton1Click(){
     
 }
 
+function handlebutton2click(){
+    var span2 = document.getElementById("span2")
+    //sends a http request to backend fastapi server
+    //server responds with whats stored in sqlite database in json format
+    //.map: loops through each entry in the array and html string is created for each one of them
+    //.join joins the html strings with line breaks in between 
+    fetch("http://127.0.0.1:8000/history",{
+        method:"GET"
+    })
+    .then(response=>response.json())
+    .then(data=>{
+        if (data.length === 0){
+            span2.innerHTML = "No past ideas found."
+            return;
+        }
+        //using .map() to loop through the array
+        span2.innerHTML = data.map(entry=>`
+            <div>
+                <strong>${entry.name}</strong> (${entry.cuisine}):
+                ${entry.menu_items}
+            </div>
+        `).join("<br>");
+    })
+    .catch(error=>{
+        span2.innerHTML = "failed to load history"
+        console.error(error)
+
+    });        
+    
+}
+
 function run(){
     var button1 = document.getElementById("button1");
     button1.addEventListener("click",handleButton1Click);
+    button2.addEventListener("click",handlebutton2click);
 
 }
 document.addEventListener("DOMContentLoaded",run)
